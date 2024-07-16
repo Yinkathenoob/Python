f16cca5396771894d08610ed327e270a1e25c6d4
Yinkathenoob <yolaonipekun@gmail.com>
2024-07-16 14:07:42 +0100

    added file Sorter

--- def decode(message):.py
+++ def decode(message):.py
@@ -1,8 +1,8 @@
-def decode(message):
+def decode(message:str):
     message = input("Enter your encoded message: ")
     reversed_message = message[::-1]
     final_message = "".join([i for i in reversed_message if i.isalpha()])
-    print(final_message)
+    print("The encoded message is: ", final_message)
 
 
-decode("")
+decode()
--- random number game.py
+++ random number game.py
@@ -1,31 +1,42 @@
 from random import randint
 
 
-
 num = randint(0,100)
 
 
 def guess_num(guess:int):
-    guess = int(input("Enter a random number: "))
-    should_continue = True
-    attempts = 0
+    
+    choose_difficulty = input("Select difficulty: Easy, Medium or Hard ")
+    
+    if choose_difficulty == "easy":
+    
+        guess = int(input("Enter a random number: "))
+        should_continue = True
+        attempts = 0
 
-    while should_continue:
-        attempts += 1
+        while should_continue:
+            attempts += 1
 
-        print(f"Number of attempts: ", attempts)
-        if guess == num:
-            print("You have guessed the correct number!")
-            should_continue = False
-        elif guess > num:
-            print("Your guess is too high")
-            guess_num(1)
-        elif guess < num:
-            print("Your guess is too low")
-            guess_num(1)
+            print(f"Number of attempts: ", attempts)
+            if guess == num:
+                print("You have guessed the correct number!")
+                should_continue = False
+            elif guess > num:
+                print("Your guess is too high")
+                guess_num(1)
+            elif guess < num:
+                print("Your guess is too low")
+                guess_num(1)
+            if attempts == 10:
+                print("Max number of attempts reached! The game will end now")
+                exit()
+    
+    elif choose_difficulty == "hard":
+        guess = int(input("Enter a random number: "))
+        should_continue = True
+        attempts = 0
+                
 guess_num(1)
-
-
 
 
 
